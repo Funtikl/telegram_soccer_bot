13fef3b19a9bffc24062f0b151a1fc1db9e1ff74
Fuad Rustamzade <fuadrustamzade@Fuads-Air.STELFIBER>
2021-09-19 13:57:23 +0400

    bos oyun

--- main.py
+++ main.py
@@ -31,7 +31,7 @@ def handle_message(update, context):
     text = update.message.text
     text = text.lower()
     response = club(text)
-    if response == '':
+    if not response:
         update.message.reply_text('Bu oyun serverdə yoxdur')
     update.message.reply_text(response)
 
