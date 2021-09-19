7b64fabcbb111c131d48188e36a0886695340a26
Fuad Rustamzade <fuadrustamzade@Fuads-Air.STELFIBER>
2021-09-19 14:01:10 +0400

    bos oyun

--- main.py
+++ main.py
@@ -31,8 +31,9 @@ def handle_message(update, context):
     text = update.message.text
     text = text.lower()
     response = club(text)
+    print(response)
     if not response:
-        update.message.reply_text('Bu oyun serverdə yoxdur')
+        update.message.reply_text('Bu klubun oyunu hələ ki serverdə yoxdur. Daha sonra yoxlayın.')
     update.message.reply_text(response)
 
 def main():
